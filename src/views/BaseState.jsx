485ee083c3a616f7d82c855cfe911685c97cdcd2
Blake Winton <bwinton@Persona.local>
2014-11-06 10:45:12 -0500

    Add the url bar to the toolbar.

--- src/views/BaseState.jsx
+++ src/views/BaseState.jsx
@@ -11,7 +11,7 @@ module.exports = React.createClass({
     return (
       <div className="StateWrapper">
         <h3 className="higher"><span className="counter">{ this.props.index + 1 }</span> { this.props.name }</h3>
-        <div className="Toolbar"><div className={"sharing " + this.props.sharing}></div></div>
+        <div className="Toolbar"><div className={"sharing " + this.props.sharing}></div><div className="end"></div></div>
         <div className="PanelWrapper">
           { this.props.children }
         </div>
