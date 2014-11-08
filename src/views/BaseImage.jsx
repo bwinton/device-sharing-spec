7f4b60c3ec7370c840e0f06cc1622ea8adcc64ea
Blake Winton <bwinton@Persona.local>
2014-11-07 19:19:22 -0500

    Fix the BaseImage to not include the Panel or Toolbar.

--- src/views/BaseImage.jsx
+++ src/views/BaseImage.jsx
@@ -5,9 +5,10 @@ module.exports = React.createClass({
   render: function() {
     console.log(this.props.data);
     return (
-      <BaseState name={ this.props.name } index={ this.props.index } sharing={ this.props.data.sharing } >
-        <div className={ "FullImage " + this.props.data.image }/>
-      </BaseState>
+      <div className="StateWrapper">
+        <h3 className="higher"><span className="counter">{ this.props.index + 1 }</span> { this.props.name }</h3>
+        <FullImage data={ this.props.data }/>
+      </div>
     );
   }
 });
