6cd81de7ba83e3ae2e34cb5d4d78fc34603d58ca
Blake Winton <bwinton@Persona.local>
2014-11-27 11:15:48 -0500

    Add a full-screen view instead of the Notes.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 /** @jsx React.DOM */
-Marked = require('marked');
-
-_users = require('./models/users.js');
-STRINGS = require('./models/strings.js')
-Utils = require('./utils/utils.js');
-getTimeFromRange = Utils.getTimeFromRange;
-TableOfContents = require('./views/TableOfContents.jsx');
-NoteView = require('./views/NoteView.jsx');
-
-// Supporting views.
-Window = require('./views/Window.jsx');
 
 // Main views.
-BaseImage = require('./views/BaseImage.jsx');
-GlobalDoorhanger = require('./views/GlobalDoorhanger.jsx');
 SharingDoorhanger = require('./views/SharingDoorhanger.jsx');
 
 moment.lang('en', {
@@ -27,113 +14,41 @@ moment.lang('en', {
   }
 });
 
-var states = [
-  {
-    name: 'The new sharing indicator',
-    slug: 'new-sharing-indicator',
-    view: BaseImage,
-    data: {image: 'new-sharing-indicator'}
-  },
-  {
-    name: 'The new doorhanger',
-    slug: 'new-doorhanger',
-    view: SharingDoorhanger,
-    data: {
-      sharing: 'requested',
-      image: 'new-doorhanger'
-    }
-  },
-  {
-    name: 'Sharing enabled',
-    slug: 'after-sharing',
-    view: SharingDoorhanger,
-    data: {
-      sharing: 'enabled',
-      image: 'after-sharing'
-    }
-  },
-  {
-    name: 'Additional permissions',
-    slug: 'additional-permissions',
-    view: SharingDoorhanger,
-    data: {
-      sharing: 'enabled-2',
-      image: 'additional-permissions'
-    }
-  },
-  {
-    name: 'Multiple sites',
-    slug: 'multiple-sites',
-    view: SharingDoorhanger,
-    data: {
-      sharing: 'enabled-3',
-      image: 'multiple-sites'
-    }
-  },
-  {
-    name: 'Screensharing dropdown',
-    slug: 'screensharing-dropdown',
-    view: SharingDoorhanger,
-    data: {
-      sharing: 'enabled-3',
-      image: 'screensharing-dropdown'
-    }
-  },
-  {
-    name: 'Global doorhanger',
-    slug: 'global-doorhanger',
-    view: GlobalDoorhanger,
-    data: {
-      sharing: 'enabled-3',
-      images: [
-      'global-doorhanger-top',
-      'global-doorhanger-middle',
-      'global-doorhanger-bottom'
-      ]
-    }
-  },
-  {
-    name: 'Single doorhanger',
-    slug: 'single-doorhanger',
-    view: GlobalDoorhanger,
-    data: {
-      sharing: 'enabled-3',
-      images: ['global-doorhanger-single']
-    }
+var state = {
+  name: 'The new doorhanger',
+  slug: 'new-doorhanger',
+  view: SharingDoorhanger,
+  data: {
+    sharing: 'requested',
+    image: 'new-doorhanger'
   }
-];
+};
+
+var screendata = {
+  image: 'full-screen'
+}
 
 setTimeout(function(){
-  React.renderComponent(<TableOfContents items={states} />, $('#toc')[0]);
-  _.each(states, function(state, index){
+  var el = $('<div/>', {
+    class: 'component-wrapper',
+    id: state.slug
+  })[0];
 
-    var el = $('<div/>', {
-      class: 'component-wrapper',
-      id: state.slug
-    })[0];
+  var viewEl = $('<div/>', {
+    class: 'View'
+  })[0];
 
-    var viewEl = $('<div/>', {
-      class: 'View'
-    })[0];
+  var screenEl = $('<div/>', {
+    class: 'NoteWrapper'
+  })[0];
 
-    var noteEl = $('<div/>', {
-      class: 'NoteWrapper'
-    })[0];
+  $(el).append(viewEl);
 
-    $(el).append(viewEl);
+  $('#wrapper').append(el);
 
-    $('#wrapper').append(el);
-
-    var View = state.view
-    $.get('./notes/' + state.slug + '.md').success(function(data){
-      React.renderComponent(<View data={state.data} error={state.error} index={index} name={state.name} slug={state.slug} />, viewEl);
-      var notes = Marked(data);
-      $(el).append(noteEl)
-      React.renderComponent(<NoteView note={notes} />, noteEl);
-    }).error(function(error){
-      React.renderComponent(<View data={state.data} items={_users} error={state.error} index={index} name={state.name} slug={state.slug} />, viewEl);
-    });
-  })
+  React.renderComponent(<SharingDoorhanger data={state.data} error={state.error} name={state.name} slug={state.slug} />, viewEl);
+  $(el).append(screenEl)
+  React.renderComponent(<FullImage data={screendata} />, screenEl);
 
   $('.tip').tipr({
     mode: 'top',
